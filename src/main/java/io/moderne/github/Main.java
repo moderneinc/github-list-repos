cca6e1b133ee80be93350c37dba783bc86c295f6
Jonathan Schneider <jkschneider@gmail.com>
2023-02-15 15:59:41 -0600

    Add endpoint, fix org name

--- src/main/java/io/moderne/github/Main.java
+++ src/main/java/io/moderne/github/Main.java
@@ -8,14 +8,17 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         String ghOrigin = args.length == 0 ? "github.com" : args[0];
-        GitHub github = GitHubBuilder.fromPropertyFile().build();
+        String ghEndpoint = args.length < 2 ? "https://api.github.com" : args[1];
+        GitHub github = GitHubBuilder.fromPropertyFile()
+                .withEndpoint(ghEndpoint)
+                .build();
 
         try (FileWriter writer = new FileWriter("repos.csv")) {
             PagedIterator<GHOrganization> orgs = github.listOrganizations()._iterator(10);
             while(orgs.hasNext()) {
                 GHOrganization org = orgs.next();
                 for (GHRepository repo : org.getRepositories().values()) {
-                    writer.write(ghOrigin + "," + org.getName() + "/" + repo.getName() + "," + repo.getDefaultBranch() + "\n");
+                    writer.write(ghOrigin + "," + org.getLogin() + "/" + repo.getName() + "," + repo.getDefaultBranch() + "\n");
                 }
             }
         }
